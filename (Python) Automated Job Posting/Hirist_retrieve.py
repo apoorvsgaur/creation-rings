aeaa37d9d459d6cb6260ef331bf2ef617008a1aa
apoorvsgaur <apoorvsgaur@gmail.com>
2015-07-27 19:55:38 +0530

    Hirist Files downloading updated

--- (Python) Automated Job Posting/Hirist_retrieve.py
+++ (Python) Automated Job Posting/Hirist_retrieve.py
@@ -3,6 +3,8 @@ from bs4 import BeautifulSoup
 import re
 import urllib
 import os
+import mimetypes
+import re
 
 def hirist_retrieve():
   br = mechanize.Browser()
@@ -15,22 +17,39 @@ def hirist_retrieve():
   br.addheaders = [('User-agent', 'Chrome')]
   br.open('http://www.hirist.com/recruiter/jobpost.php')
   br.select_form(name="recruiterRegLogin")
-  br['email'] = 'apoorv@venturesity.com'
-  br['password'] = 'venturesity@123'
+  br['email'] = 'saranya@venturesity.com'
+  br['password'] = 'Venturesity@123'
   br.submit()
   response = br.open('http://www.hirist.com/recruiter/recruiter.php')
   soup = response.read()
   soup = BeautifulSoup(soup, "html.parser")
+  j = 1
+  k = 0
   for link in soup.find_all('a', title="Total Apply"):
-     response = br.open(link.get('href'))
-     soup = BeautifulSoup(response, "html.parser")
-     i = 0
-     for link in soup.find_all('a'):
-         if 'resumedownload' in link.get('href'):
-             print link.get('href')
-             i = i + 1
-             response = br.open(link.get('href'))
-             with open('resume.txt', 'w') as f:
-                 f.write(response.read())
-                 x++
+      if k < j:
+         names_list = []
+         response = br.open(link.get('href'))
+         soup = BeautifulSoup(response, "html.parser")
+         name_of_job = soup.find_all('h2', {"class":"hidden-xs"})[0].text
+         if not os.path.exists(os.path.join(os.path.expanduser("~/Desktop/Job Postings/Hirist/" + name_of_job.strip().replace("/","_")))):
+             os.makedirs(os.path.join(os.path.expanduser("~/Desktop/Job Postings/Hirist/" + name_of_job.strip().replace("/","_"))))
+         for names in soup.find_all('p', {"class" : "smallviewtext"}):
+             string = str(names).split("<br/>")[0].split()
+             names_list.append(' '.join(string[2:]))
+         print names_list
+         i = 0
+         for link in soup.find_all('a'):
+             if 'resumedownload' in link.get('href'):
+             #print link.get('href')
+                  filename, headers = br.retrieve(link.get('href'))
+                  content_type = headers.get('Content-Type', '').split(";")[0]
+                  extension = mimetypes.guess_extension(content_type)
+                  br.retrieve(link.get('href'), os.path.expanduser("~/Desktop/Job Postings/Hirist/" + name_of_job.strip().replace("/","_") + "/" + names_list[i] + extension))[0]
+                  i = i + 1
+                 #print extension
+
+                 #with open('resume.txt', 'w') as f:
+                    #   f.write(response.read())
+      else: 
+              break
 hirist_retrieve()
