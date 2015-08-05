409db27cb40840837890dfc5ea5f0e5c85f73b44
apoorvsgaur <apoorvsgaur@gmail.com>
2015-08-05 16:09:37 +0530

    Calculating personal score completed

--- Python-Github-Score-Calculate/user_score_calculate.py
+++ Python-Github-Score-Calculate/user_score_calculate.py
@@ -1,41 +1,67 @@
 import json
 import requests
+import math
 
 def github_score(user):
-    try:
-        url = 'https://api.github.com/users/' + str(user) + '/repos?page=1&per_page=1000&access_token=c9e62a0705e69d2d5ed041d1a1c27d853aded88f'
-        html = requests.get(url).text
-    except:
-#        print user
-        return user
-    #print url
+    url = 'https://api.github.com/users/' + user + '?access_token=c9e62a0705e69d2d5ed041d1a1c27d853aded88f'
+    html = requests.get(url).text
     data = json.loads(html)
-    #print data
+    followers_score = data['followers']
+    page_count = 1
     repo_score = 0
     number_lines = 0
     fork_score = 0
     star_score = 0
     commit_score = 0
-    for x in data:
+    score = 0
+    forked_repo_score = 0
+    while(True):
         try:
-           print x['full_name']
-           if x['fork'] == False:
-               repo_score = repo_score + 1
-               fork_score = fork_score + x['forks']
-               star_score = star_score + x['stargazers_count']
-           else:
-                url = 'https://api.github.com/repos/' + x['full_name'] + '/stats/contributors'
-                #print url
+                url = 'https://api.github.com/users/' + str(user) + '/repos?page=' + str(page_count) + '&per_page=1000&access_token=c9e62a0705e69d2d5ed041d1a1c27d853aded88f'
                 html = requests.get(url).text
                 data = json.loads(html)
-                commit_repo = 0
-                commit_repo = data[0]['total']
-                for x in data[0]['weeks']:
-                    number_lines = number_lines + x['a'] + x['d']
+                if (len(data) == 0):
+                    break
+                page_count = page_count + 1
 
+        except:
+                pass
+            #print url
 
-        except:#    print score
-            return score
+            #print data
+        for x in data:
+                try:
+                   print x['full_name']
+                   if x['fork'] == False:
+                       repo_score = repo_score + 1
+                       fork_score = fork_score + x['forks']
+                       star_score = star_score + x['stargazers_count']
+                   else:
+                        try:
+                            url = 'https://api.github.com/repos/' + x['full_name'] + '/stats/contributors?access_token=c9e62a0705e69d2d5ed041d1a1c27d853aded88f'
+                            #print url
+                            html = requests.get(url).text
+                            data = json.loads(html)
+                            for y in data[0]['weeks']:
+                                number_lines = number_lines + y['a'] + y['d']
 
-score = github_score("cyberhck")
+                            commit_repo = 0
+                            commit_repo = data[0]['total']
+                            #print data[-1]['author']['login']
+                            #print x['full_name'].split("/")[-1]
+                            url = 'https://api.github.com/repos/' + data[-1]['author']['login'] + "/" + x['full_name'].split("/")[-1] + '?access_token=c9e62a0705e69d2d5ed041d1a1c27d853aded88f'
+                            #print url
+                            html = requests.get(url).text
+                            data = json.loads(html)
+                            forks_repo = 0
+                            forks_repo = data['forks']
+                            forked_repo_score = forked_repo_score + math.sqrt(commit_repo*forks_repo)
+                        except:
+                            pass
+                except:#    print score
+                    print x
+
+    print (followers_score,repo_score,fork_score,star_score,forked_repo_score,number_lines)
+
+github_score("hemanth")
 #print score
